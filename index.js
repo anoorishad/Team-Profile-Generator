6db9d3e89d44d38eb8cbc6afcffd84a1bd076792
Andrew Noorishad <anoorishad@me.com>
2022-05-01 15:53:06 -0700

    updated readme

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const generateHTML = require('./util/generateHtml')
+const generateHtml = require('./util/generateHtml')
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
@@ -55,12 +55,43 @@ const internInfo = ()=>{
         },
         {
             type: 'input',
-            name: 'officeNumber',
+            name: 'school',
             message: 'Please enter name of school intern is from:',
         },
     ]).then(ans=>{
         const intern = new Intern(ans.name,ans.id,ans.email,ans.school);
         employees.push(intern);
+        options();
+    })
+};
+
+
+
+const engineerInfo = ()=>{
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter engineer name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter engineer employee ID:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter engineer email address:',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter engineer GitHub username:',
+        },
+    ]).then(ans=>{
+        const engineer = new Engineer(ans.name,ans.id,ans.email,ans.github);
+        employees.push(engineer);
         options();
     })
 };
@@ -78,14 +109,26 @@ const options = ()=>{
         }
 
     ]).then(ans=>{
-        switch (ans.selection) {
+        switch (ans.choices) {
             case 'Add Intern':
                 internInfo();
-                options();
-
+                
                 break;
             case 'Add Engineer':
-                
+                engineerInfo();
+
+                break;
+            case 'Finish Building Team':
+                fs.writeFileSync("./util/index.html", generateHtml(employees));
+                console.log("File has been written!")
+
+                break;
+
+            default:
+                console.log("Thanks for using the team profile creator!");
+                break;
+
+
         }
 
     })
@@ -96,4 +139,3 @@ const options = ()=>{
 
 
 managerInfo();
-// pick up at 1:27 on video
